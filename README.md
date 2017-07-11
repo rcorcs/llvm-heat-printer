1249b948ee7706396713ef44db25c1e16856374d
Rodrigo Caetano Rocha <rcor.cs@gmail.com>
2017-07-11 18:41:57 +0100

    commit of the first version of the heat-cfg printer

---
+++ README.md
@@ -0,0 +1,1 @@
+# llvm-heat-printer
---
+++ src/HeatCFGPrinter.h
@@ -0,0 +1,51 @@
+//===-- CFGPrinter.h - CFG printer external interface -----------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines a 'dot-heat-cfg' analysis pass, which emits the
+// cfg.<fnname>.dot file for each function in the program, with a graph of the
+// CFG for that function coloured with heat map depending on the basic block
+// frequency.
+//
+// This file defines external functions that can be called to explicitly
+// instantiate the CFG printer.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_ANALYSIS_HEATCFGPRINTER_H
+#define LLVM_ANALYSIS_HEATCFGPRINTER_H
+
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+
+using namespace llvm;
+
+namespace {
+
+class HeatCFGPrinterPass : public ModulePass {
+public:
+  static char ID;
+  HeatCFGPrinterPass() : ModulePass(ID) {}
+
+  void getAnalysisUsage(AnalysisUsage &AU) const;
+  bool runOnModule(Module &M) override;
+};
+
+class HeatCFGOnlyPrinterPass : public ModulePass {
+public:
+  static char ID;
+  HeatCFGOnlyPrinterPass() : ModulePass(ID) {}
+
+  void getAnalysisUsage(AnalysisUsage &AU) const;
+  bool runOnModule(Module &M) override;
+
+};
+
+}
+
+#endif
